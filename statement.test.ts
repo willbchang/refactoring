1a3d32a40932d2aade34152692d978fefd103e91
Will B Chang <will.b.chang@gmail.com>
2024-06-03 14:42:58 +0800

    test: use bun for statement

---
+++ statement.test.ts
@@ -0,0 +1,15 @@
+// @ts-ignore
+import { expect, test } from 'bun:test'
+import { Invoices, Plays } from './data'
+import { statement } from './statement'
+
+test('statement', () => {
+  const result = `Statement for BigCo
+ Hamlet: $650.00 (55 seats)
+ As You Like It: $580.00 (35 seats)
+ Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`
+  expect(statement(Invoices[0], Plays)).toBe(result)
+})
