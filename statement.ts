e2013f4fb920002e16cbf8a11835b2d0bb5a1991
Will B Chang <will.b.chang@gmail.com>
2024-06-05 14:51:05 +0800

    refactor: init result before return

--- statement.ts
+++ statement.ts
@@ -5,11 +5,13 @@ export function statement(invoice: Invoice, plays: Plays) {
   }
   return getPlainText(data)
 
-  function getStatementPerformance(performance: Performance): StatementPerformance {
-    return {
+  function getStatementPerformance(performance: Performance) {
+    let result: StatementPerformance = {
       ...performance,
       play: getPlay(performance),
     }
+
+    return result
   }
 
   function getPlay(perf: Performance) {
@@ -28,27 +30,6 @@ function getPlainText(data: Statement) {
 
   return result
 
-  function getAmount(perf: StatementPerformance) {
-    let result = 0
-    switch (perf.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (perf.audience > 30) {
-          result += 1000 * (perf.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30000
-        if (perf.audience > 20) {
-          result += 10000 + 500 * (perf.audience - 20)
-        }
-        result += 300 * perf.audience
-        break
-      default:
-        throw new Error(`unknown type: ${perf.play.type}`)
-    }
-    return result
-  }
 
   function usd(amount: number) {
     return new Intl.NumberFormat('en-US', {
