c17d6bcdb0c1d4e25584f57eee5b5996a94e3ef1
Will B Chang <will.b.chang@gmail.com>
2024-06-06 11:10:39 +0800

    test: createStatementData

--- statement.test.ts
+++ statement.test.ts
@@ -1,7 +1,32 @@
 // @ts-ignore
-import { expect, test } from 'bun:test'
+import { expect, describe, test } from 'bun:test'
+import { createStatementData } from './createStatementData'
 import { Invoices, Plays } from './data'
 import { statement } from './statement'
+
+describe('createStatementData', () => {
+  const data = createStatementData(Invoices[0], Plays)
+
+  test('tragedy', () => {
+    const target = data.performances.find(item => item.play.type === 'tragedy')!
+    expect(target.amount).toBe(65000)
+    expect(target.volumeCredits).toBe(25)
+  })
+
+  test('comedy', () => {
+    const target = data.performances.find(item => item.play.type === 'comedy')!
+    expect(target.amount).toBe(58000)
+    expect(target.volumeCredits).toBe(12)
+  })
+
+  test('totalVolumeCredits', () => {
+    expect(data.totalVolumeCredits).toBe(47)
+  })
+
+  test('totalAmount', () => {
+    expect(data.totalAmount).toBe(173000)
+  })
+})
 
 test('statement', () => {
   const result = `Statement for BigCo
