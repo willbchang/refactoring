d4a021436953188b828be3418774625abc8d288b
Will B Chang <will.b.chang@gmail.com>
2024-06-06 10:46:30 +0800

    refactor: move function to file

---
+++ createStatementData.ts
@@ -0,0 +1,68 @@
+export function createStatementData(invoice: Invoice, plays: Plays) {
+  const data: Statement = {
+    customer: invoice.customer,
+    performances: invoice.performances.map(getStatementPerformance),
+    totalAmount: 0,
+    totalVolumeCredits: 0,
+  }
+
+  data.totalAmount = getTotalAmount()
+  data.totalVolumeCredits = getTotalVolumeCredits()
+
+  return data
+
+  function getStatementPerformance(performance: Performance) {
+    let result: StatementPerformance = {
+      ...performance,
+      play: getPlay(performance),
+      amount: 0,
+      volumeCredits: 0,
+    }
+
+    result.amount = getAmount(result)
+    result.volumeCredits = getVolumeCredits(result)
+    return result
+  }
+
+  function getPlay(perf: Performance) {
+    return plays[perf.playID]
+  }
+
+  function getAmount(perf: StatementPerformance) {
+    let result = 0
+    switch (perf.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (perf.audience > 30) {
+          result += 1000 * (perf.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (perf.audience > 20) {
+          result += 10000 + 500 * (perf.audience - 20)
+        }
+        result += 300 * perf.audience
+        break
+      default:
+        throw new Error(`unknown type: ${perf.play.type}`)
+    }
+    return result
+  }
+
+  function getVolumeCredits(perf: StatementPerformance) {
+    let result = 0
+    result += Math.max(perf.audience - 30, 0)
+    if ('comedy' === perf.play.type) result += Math.floor(perf.audience / 5)
+
+    return result
+  }
+
+  function getTotalAmount() {
+    return data.performances.reduce((sum, item) => sum + item.amount, 0)
+  }
+
+  function getTotalVolumeCredits() {
+    return data.performances.reduce((sum, item) => sum + item.volumeCredits, 0)
+  }
+}
